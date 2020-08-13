9ac574057d6e26b860df4c40a69827d16067678d
Ayokunle Paul <ayokunle@ulesson.com>
2020-08-13 18:42:37 +0100

    Add docker file

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Fatal(r.Run(":3000"))
+}
